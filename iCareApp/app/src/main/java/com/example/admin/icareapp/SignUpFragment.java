ac1f2d1e6443f5d57ba45363228a8aa08a40a699
Long Vu <Long Vu>
2016-10-22 00:15:58 +0300

    sign up fixed

--- iCareApp/app/src/main/java/com/example/admin/icareapp/SignUpFragment.java
+++ iCareApp/app/src/main/java/com/example/admin/icareapp/SignUpFragment.java
@@ -3,6 +3,7 @@ package com.example.admin.icareapp;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -24,21 +25,30 @@ public class SignUpFragment extends Fragment implements View.OnClickListener, Vi
     private FragmentTransaction frag_transaction;
     private EditText login_id;
     private EditText password;
-    private RelativeLayout su_header;
-    private boolean validID, validPW;
+    private EditText password_confirm;
+    private TextInputLayout login_id_container;
+    private TextInputLayout password_container;
+    private TextInputLayout password_confirm_container;
+    private boolean validID, validPW, validPWConf;
+    private String get_login_id;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View view = inflater.inflate(R.layout.testlayout, container, false);
 
         validID = false;
         validPW = false;
+        validPWConf = false;
 
         ImageButton back_button = (ImageButton) view.findViewById(R.id.su_back_button);
         Button sign_in_button = (Button) view.findViewById(R.id.su_sign_up_button);
         login_id = (EditText) view.findViewById(R.id.su_login_id_input);
         password = (EditText) view.findViewById(R.id.su_password_input);
-        su_header = (RelativeLayout) view.findViewById(R.id.su_header);
+        password_confirm = (EditText) view.findViewById(R.id.su_password_confirm_input);
+        login_id_container = (TextInputLayout) view.findViewById(R.id.su_login_container);
+        password_container = (TextInputLayout) view.findViewById(R.id.su_password_container);
+        password_confirm_container = (TextInputLayout) view.findViewById(R.id.su_password_confirm_container);
+
 
         back_button.setOnClickListener(this);
         sign_in_button.setOnClickListener(this);
@@ -46,6 +56,7 @@ public class SignUpFragment extends Fragment implements View.OnClickListener, Vi
         //password.setOnFocusChangeListener(this);
         login_id.addTextChangedListener(this);
         password.addTextChangedListener(this);
+        password_confirm.addTextChangedListener(this);
 
         return view;
     }
@@ -57,8 +68,35 @@ public class SignUpFragment extends Fragment implements View.OnClickListener, Vi
                 getFragmentManager().popBackStack();
                 break;
             case R.id.su_sign_up_button:
-                if (validID && validPW)
-                    new QueryDatabase(getActivity(), this).execute("insert_user", "http://192.168.0.102/Insert_NewCustomer.php", login_id.getText().toString(), password.getText().toString());
+                //if (validID && validPW)
+                    //new QueryDatabase(getActivity(), this).execute("insert_user", "http://192.168.0.102/Insert_NewCustomer.php", login_id.getText().toString(), password.getText().toString());
+                if (validID && validPW && validPWConf)
+                    new QueryDatabase(getActivity(), this).execute("checkuser", "http://192.168.0.102/Select_CheckUserExistence.php", get_login_id);
+                else {
+                    if (!validID){
+                        if (login_id.getText().toString().equals(""))
+                            login_id_container.setError(getString(R.string.username_null));
+                        else
+                            login_id_container.setError(getString(R.string.username_requirement));
+                        login_id_container.setErrorEnabled(true);
+                    }
+
+                    if (!validPW){
+                        if (password.getText().toString().equals(""))
+                            password_container.setError(getString(R.string.password_null));
+                        else
+                            password_container.setError(getString(R.string.password_requirement));
+                        password_container.setErrorEnabled(true);
+                    }
+
+                    if (!validPWConf){
+                        if (password_confirm.getText().toString().equals(""))
+                            password_confirm_container.setError(getString(R.string.password_confirm_null));
+                        else
+                            password_confirm_container.setError(getString(R.string.password_confirm_requirement));
+                        password_confirm_container.setErrorEnabled(true);
+                    }
+                }
                 break;
             default:
                 break;
@@ -70,17 +108,16 @@ public class SignUpFragment extends Fragment implements View.OnClickListener, Vi
         String status = (String) o;
 
         if (status.equals("NotExist")) {
-            login_id.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_person_black_24dp, 0, R.drawable.ic_verified_user_black_24dp, 0);
-            validID = true;
+            //login_id.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_person_black_24dp, 0, R.drawable.ic_verified_user_black_24dp, 0);
+            //validID = true;
+            new QueryDatabase(getActivity(), this).execute("insert_user", "http://192.168.0.102/Insert_NewCustomer.php", login_id.getText().toString(), password.getText().toString());
         } else if (status.equals("Exist")){
-            login_id.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_person_black_24dp, 0, R.drawable.ic_error_outline_black_24dp, 0);
+            login_id.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_user, 0, R.drawable.ic_invalid_input, 0);
+            login_id_container.setError(getString(R.string.username_invalid));
+            login_id_container.setErrorEnabled(true);
             validID = false;
         } else if (status.equals("Inserted")){
             System.out.println("Instert successfully");
-            frag_transaction = getFragmentManager().beginTransaction();
-            frag_transaction.replace(R.id.wel_fragment_container, new InfoFragment());
-            frag_transaction.addToBackStack(null);
-            frag_transaction.commit();
         } else if (status.equals("Failed")){
             System.out.println("Insert failed");
         }
@@ -96,18 +133,21 @@ public class SignUpFragment extends Fragment implements View.OnClickListener, Vi
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
         if (login_id.getText().hashCode() == s.hashCode()){ //Check username
-            String username = s.toString();
-            username.trim();
-            if (!username.equals("")){
-                if (username.matches(login_in_requirement))
-                    new QueryDatabase(getActivity(), this).execute("checkuser", "http://192.168.0.102/Select_CheckUserExistence.php", username);
-                else {
-                    login_id.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_person_black_24dp, 0, R.drawable.ic_error_outline_black_24dp, 0);
+            get_login_id = s.toString();
+            get_login_id.trim();
+            if (!get_login_id.equals("")){
+                if (get_login_id.matches(login_in_requirement)) {
+                    //new QueryDatabase(getActivity(), this).execute("checkuser", "http://192.168.0.102/Select_CheckUserExistence.php", username);
+                    login_id.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_user, 0, R.drawable.ic_valid_input, 0);
+                    login_id_container.setErrorEnabled(false);
+                    validID = true;
+                } else {
+                    login_id.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_user, 0, R.drawable.ic_invalid_input, 0);
                     validID = false;
                 }
             }
             else {
-                login_id.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_person_black_24dp, 0, 0, 0);
+                login_id.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_user, 0, 0, 0);
                 validID = false;
             }
         } else if (password.getText().hashCode() == s.hashCode()){ //Check password
@@ -115,17 +155,36 @@ public class SignUpFragment extends Fragment implements View.OnClickListener, Vi
             pw.trim();
             if (!pw.equals("")){
                 if (pw.matches(password_requirement)) {
-                    password.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_vpn_key_black_24dp, 0, R.drawable.ic_verified_user_black_24dp, 0);
+                    password.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_password, 0, R.drawable.ic_valid_input, 0);
+                    password_container.setErrorEnabled(false);
                     validPW = true;
                 }
                 else{
-                    password.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_vpn_key_black_24dp, 0 , R.drawable.ic_error_outline_black_24dp, 0);
+                    password.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_password, 0 , R.drawable.ic_invalid_input, 0);
                     validPW = false;
                 }
             }
             else{
-                password.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_vpn_key_black_24dp, 0 , 0, 0);
+                password.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_password, 0 , 0, 0);
                 validPW = false;
+            }
+        } else if (password_confirm.getText().hashCode() == s.hashCode()){ //Check password
+            String pwconf = s.toString();
+            pwconf.trim();
+            if (!pwconf.equals("")){
+                if (pwconf.equals(password.getText().toString())) {
+                    password_confirm.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_pw_confirm, 0, R.drawable.ic_valid_input, 0);
+                    password_confirm_container.setErrorEnabled(false);
+                    validPWConf = true;
+                }
+                else{
+                    password_confirm.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_pw_confirm, 0 , R.drawable.ic_invalid_input, 0);
+                    validPWConf = false;
+                }
+            }
+            else{
+                password_confirm.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_pw_confirm, 0 , 0, 0);
+                validPWConf = false;
             }
         }
     }
@@ -137,9 +196,6 @@ public class SignUpFragment extends Fragment implements View.OnClickListener, Vi
 
     @Override
     public void onFocusChange(View v, boolean hasFocus) {
-        if (hasFocus){
-            su_header.setVisibility(View.GONE);
-        }else
-            su_header.setVisibility(View.VISIBLE);
+
     }
 }
