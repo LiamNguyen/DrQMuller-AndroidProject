05e646c38041cecae4171457055c99b929562eee
Long Vu <longvh96@gmail.com>
2017-04-02 11:02:38 +0300

    Changed regex for validation

--- iCareApp/app/src/main/java/com/lanthanh/admin/icareapp/presentation/model/InputRequirement.java
+++ iCareApp/app/src/main/java/com/lanthanh/admin/icareapp/presentation/model/InputRequirement.java
@@ -5,10 +5,10 @@ package com.lanthanh.admin.icareapp.presentation.model;
  */
 
 public class InputRequirement {
-    public static final String USERNAME = "^[^.\\-@](?!.*[\\s[^\\w]])[A-Za-z0-9]{4,62}[^.\\-@]$";
-    public static final String PASSWORD = "^[^\\s](?=.*[\\d\\W])(?=.*[a-zA-Z]).{6,253}[^\\s]$";
-    public static final String NAME = "[\\p{L}\\-.\\s']{0,100}";
-    public static final String ADDRESS = "[\\p{L}\\p{N}\\-.\\s,'\"/]{0,50}";
-    public static final String EMAIL = "[\\w._\\-]+@[^@]*[^@]$";
-    public static final String PHONE = "^[+\\d][()\\d]{1,16}";
+    public static final String USERNAME = "^[A-Za-z0-9\\-\\_]{8,25}$"; //"^[^.\\-@](?!.*[\\s[^\\w]])[A-Za-z0-9]{4,62}[^.\\-@]$";
+    public static final String PASSWORD = "^[A-Za-z0-9]{8,30}$"; //"^[^\\s](?=.*[\\d\\W])(?=.*[a-zA-Z]).{6,253}[^\\s]$";
+    public static final String NAME = "^[A-Za-zÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\\s\\-.]{0,50}$"; //"[\\p{L}\\-.\\s']{0,100}";
+    public static final String ADDRESS = "^[A-Za-zÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ0-9\\s-.\\/]{0,150}$"; //"[\\p{L}\\p{N}\\-.\\s,'\"/]{0,50}";
+    public static final String EMAIL =  "^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";//"[\\w._\\-]+@[^@]*[^@]$";
+    public static final String PHONE =  "^\\+?[0-9]+[0-9\\-\\s]+\\(?[0-9]+\\)?[0-9\\-\\s]{0,12}$"; //"^[+\\d][()\\d]{1,16}";
 }
