6930e993948ef8b6ab695b6522e0c837e7c82fc7
Long Vu <longvh96@gmail.com>
2017-04-10 23:04:30 +0300

    Implement RxBinding for log in & sign up frags

--- iCareApp/app/src/main/java/com/lanthanh/admin/icareapp/presentation/welcomepage/LogInFragment.java
+++ iCareApp/app/src/main/java/com/lanthanh/admin/icareapp/presentation/welcomepage/LogInFragment.java
@@ -10,31 +10,34 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.jakewharton.rxbinding2.widget.RxTextView;
 import com.lanthanh.admin.icareapp.R;
 import com.lanthanh.admin.icareapp.presentation.application.ApplicationProvider;
+import com.lanthanh.admin.icareapp.presentation.model.InputRequirement;
 import com.lanthanh.admin.icareapp.presentation.resetpasswordpage.ResetPasswordActivity;
 import com.lanthanh.admin.icareapp.presentation.base.BaseFragment;
 import com.lanthanh.admin.icareapp.utils.GraphicUtils;
+import com.lanthanh.admin.icareapp.utils.StringUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
 
 /**
  * Created by ADMIN on 17-Oct-16.
  */
 
 public class LogInFragment extends BaseFragment<WelcomeActivityPresenter>{
-    @BindView(R.id.si_username_input)
-    TextInputEditText editUsername;
+    @BindView(R.id.si_username_input) TextInputEditText editUsername;
     @BindView(R.id.si_password_input) TextInputEditText editPassword;
-    @BindView(R.id.si_sign_in_button)
-    AppCompatButton logInButton;
-    @BindView(R.id.si_forget_pw)
-    TextView forgetPwText;
-    @BindView(R.id.si_username_container)
-    TextInputLayout editUsernameContainer;
+    @BindView(R.id.si_sign_in_button) AppCompatButton logInButton;
+    @BindView(R.id.si_forget_pw) TextView forgetPwText;
+    @BindView(R.id.si_username_container) TextInputLayout editUsernameContainer;
     @BindView(R.id.si_password_container) TextInputLayout editPasswordContainer;
+
+    private Disposable editTextDisposable;
     private Unbinder unbinder;
 
     @Override
@@ -50,27 +53,28 @@ public class LogInFragment extends BaseFragment<WelcomeActivityPresenter>{
     @Override
     public void initViews() {
         ((WelcomeActivity) getActivity()).showToolbar(true);
-        //Custom font
+
+        //Apply custom font for UI elements
         Typeface font = Typeface.createFromAsset(getActivity().getAssets(), GraphicUtils.FONT_LIGHT);
         logInButton.setTypeface(font);
-        forgetPwText.setTypeface(font);
         editUsername.setTypeface(font);
         editPassword.setTypeface(font);
         editUsernameContainer.setTypeface(font);
         editPasswordContainer.setTypeface(font);
 
+        //Set up listener for button
         logInButton.setOnClickListener(
-            view ->  {
-                ((WelcomeActivity) getActivity()).hideSoftKeyboard();
-                getMainPresenter().login(editUsername.getText().toString().trim(), editPassword.getText().toString());
-            }
+                view ->  {
+                    ((WelcomeActivity) getActivity()).hideSoftKeyboard();
+                    getMainPresenter().login(editUsername.getText().toString().trim(), editPassword.getText().toString());
+                }
         );
-        forgetPwText.setOnClickListener(
-            view -> {
-                ((WelcomeActivity) getActivity()).hideSoftKeyboard();
-                getMainPresenter().navigateActivity(ResetPasswordActivity.class);
-            }
-        );
+
+        //Observe edit texts' value
+        Observable<Boolean> usernameObservable = RxTextView.textChanges(editUsername).map(StringUtils::isNotEmpty);
+        Observable<Boolean> passwordObservable = RxTextView.textChanges(editPassword).map(StringUtils::isNotEmpty);
+        editTextDisposable =  Observable.combineLatest(usernameObservable, passwordObservable, (validUsername, validPassword) -> validUsername && validPassword)
+                                        .subscribe(valid -> logInButton.setEnabled(valid));
     }
 
     @Override
@@ -104,6 +108,7 @@ public class LogInFragment extends BaseFragment<WelcomeActivityPresenter>{
     public void onDestroy() {
         super.onDestroy();
         unbinder.unbind();
+        editTextDisposable.dispose();
     }
 }
 
